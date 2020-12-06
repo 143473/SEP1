6070b526200f9d77d445fe6c31dd771993a5fc35
AsuraEyes <t.johan100@cloud.com>
2020-12-06 10:27:01 +0100

    Added the comments

--- Classes/src/AssignedTasks.java
+++ Classes/src/AssignedTasks.java
@@ -8,21 +8,45 @@ public class AssignedTasks extends Task{
     private Mydate date;
     private AssignedEmployee assignedEmployee;
 
+    /**
+     * Three-argument constructor.
+     * @param name the student's first name
+     * @param description the student's last name
+     * @param responsibleEmployee the student's country
+     * @param assignedEmployee
+     * @param date
+     */
     public AssignedTasks(String name, String description, AssignedEmployee responsibleEmployee, AssignedEmployee assignedEmployee, Mydate date){
         super(name, description, responsibleEmployee);
         spentTime = 0;
         this.assignedEmployee = assignedEmployee;
         this.date = date;
     }
+    /**
+     * Gets the employee's date working on the task.
+     * @return the date the task was worked on
+     */
     public MyDate getDate(){
         return date.copy();
     }
+    /**
+     * Gets the employee assigned to the task.
+     * @return the employee assigned to the task
+     */
     public AssignedEmployee getAssignedEmployee(){
         return assignedEmployee.copy();
     }
+    /**
+     * Sets the employee's time spent.
+     * @param spentTime what the employee's time spent is
+     */
     public void setSpentTime(double spentTime){
         this.spentTime = spentTime;
     }
+    /**
+     * Gets the employee's time spent working.
+     * @return the student's time spent working
+     */
     public double getSpentTime() {
         return spentTime;
     }
--- Classes/src/AssignedTasksList.java
+++ Classes/src/AssignedTasksList.java
@@ -1,14 +1,30 @@
 import java.util.ArrayList;
 
+/**
+ * A class containing a list of AssignedTask objects.
+ * @author Timothy Engkar
+ * @version 1.0
+ */
 public class AssignedTasksList {
     private ArrayList<AssignedTasks> assignedTasks;
 
+    /**
+     * No-argument constructor initializing the AssignedTasksList.
+     */
     public AssignedTasksList(){
         assignedTasks = new ArrayList<AssignedTasks>();
     }
+    /**
+     * Adds an AssignedTask to the list.
+     * @param assignedTasks the assignedTask to add to the list
+     */
     public void addAssignedTask(AssignedTasks assignedTasks){
         this.assignedTasks.add(assignedTasks);
     }
+    /**
+     * Removes a Student to the list.
+     * @param assignedTasks the assignedTask to remove to the list
+     */
     public void removeAssignedTask(AssignedTasks assignedTasks){
         this.assignedTasks.remove(assignedTasks);
     }
